72e45faf8512795455ae6577af49936888f7f474
Steven Lim <128828904+nvliteesm@users.noreply.github.com>
2024-02-14 00:13:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "makasih babii";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
