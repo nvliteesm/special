baff2c9593ef347a9298ac11eb9fb13899664a9e
Steven Lim <128828904+nvliteesm@users.noreply.github.com>
2024-02-14 00:10:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -43,11 +43,11 @@ function resizeYesButton() {
 function generateMessage(noCount) {
   const messages = [
     "No",
-    "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
+    "Noo",
+    "Nooo",
+    "Noooo",
+    "Nooooo",
+    "yes!",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
